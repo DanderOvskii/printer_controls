4f3d2de6a85a35c7df95b38fc64f487f9be5d8d7
DanderOvskii <dander@roelsieg.nl>
2026-03-24 23:28:58 +0100

    OOP camera system

--- app/api/camera_api.py
+++ app/api/camera_api.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, jsonify, request, Response
-from app.camera.camera import video_stream, switch_camera
+from app.camera.camera import camera,video_stream
 from flask_login import login_required
 
 bp = Blueprint("camera",__name__,url_prefix="/api/camera")
 
-@bp.route("/video_feed",methods = ["GET", "POST"])
+@bp.route("/video_feed",methods = ["GET"])
 @login_required
 def video_feed():
     return Response(video_stream(), mimetype= 'multipart/x-mixed-replace; boundary=frame')
 
-@bp.route('/switch_camera',methods = ["GET", "POST"])
+@bp.route('/switch_camera',methods = ["GET"])
 @login_required
 def switch():
     cam_index = int(request.args.get('cam'))
-    switch_camera(cam_index)
+    camera.switch_camera(cam_index)
     return "Camera switched"
--- app/camera/camera.py
+++ app/camera/camera.py
@@ -4,34 +4,66 @@ import time
 import atexit
 import threading
 
-current_camera = 0
-video_lock = threading.Lock()
-video = cv2.VideoCapture(current_camera, cv2.CAP_ANY)
-video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-atexit.register(lambda: video.release())
-if not video.isOpened():
-    print("Camera failed to open")
+class Camera:
+    def __init__(self, current_camera =1):
+        self.current_camera = current_camera
+        self.video = cv2.VideoCapture(current_camera, cv2.CAP_ANY)
+        self.video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+        self.video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+        self.video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+        if not self.video.isOpened():
+            print("camera failed to open")
 
+        self.frame = None
+        self.lock = threading.Lock()
+        self.thread = threading.Thread(target=self._video_stream)
+        self.thread.deamon = True
+        self.thread.start()
+        atexit.register(self.turn_off)
+
+    def _video_stream(self):
+        while True:
+            with self.lock:
+                if self.video is None:
+                    continue
+                ret, frame = self.video.read()
+                if not ret:
+                    print("failed to get frame")
+                    continue
+                self.frame = frame
+
+    def get_frame(self):
+        with self.lock:
+            return self.frame
+
+    def switch_camera(self,index):
+        print("cameraswich")
+        with self.lock:
+            if self.video is not None:
+                self.turn_off
+            self.current_camera = index
+            self.video = cv2.VideoCapture(self.current_camera,cv2.CAP_ANY)
+
+
+    def turn_off(self):
+        with self.lock:
+            if self.video is not None:
+                self.video.release()
+
+camera = Camera()
 def video_stream():
-    global video
     while True:
-        with video_lock:
-            ret, frame = video.read()
-            if not ret:
-                print("failed to get frame")
-                break
-            ret, buffer = cv2.imencode('.jpeg', frame)
-            frame_bytes = buffer.tobytes()
-        yield (b'--frame\r\n' b'Content-type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        frame = camera.get_frame()
+        if frame is None:
+            continue
 
-def switch_camera(index):
-    print("cameraswich")
-    global video, current_camera
-    with video_lock:
-        video.release()
-        current_camera = index
-        print("current cam", current_camera)
-        video = cv2.VideoCapture(current_camera)
+        ret, buffer = cv2.imencode('.jpeg', frame)
+        if not ret:
+            continue
 
+        frame_bytes = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-type: image/jpeg\r\n\r\n' +
+               frame_bytes +
+               b'\r\n')
